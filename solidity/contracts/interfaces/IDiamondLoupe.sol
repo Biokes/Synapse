b10e1aec52dee557ab12b153f69d77be9783c3f3
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-09 23:54:26 +0100

    update(contracts): update IDiamondLoupe interface

---
+++ solidity/contracts/interfaces/IDiamondLoupe.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+interface IDiamondLoupe{
+
+    struct Facet{
+        address facetAddress;
+        bytes4[] functionSelectors;
+    }
+
+    function facets() external view returns (Facet[] memory facets_);
+    function facetFunctionSelectors(address _facet) external view returns (bytes4[] memory facetFunctionSelectors);
+    function facetAddresses() external view returns (address[] memory facetAddresses);
+    function facetAddress(bytes4 _functionSelector) external view returns (address facetAddress);
+    function supportsInterface(bytes4 _interfaceId) external view returns (bool isSupported);
+    
+}
