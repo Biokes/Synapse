40c8083fec553d89d9b5a37a5c47eb5ff2c2bc7c
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-09 23:40:31 +0100

    update(contracts): added facet diamonDtorage

--- solidity/contracts/libs/SynapseUtils.sol
+++ solidity/contracts/libs/SynapseUtils.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.28;
 
 library SynapseUtils{
+
     bytes32 constant SYNAPSE_STORAGE = keccak256("Synapse.diamond.storage");
+    
     struct DiamonsStorage {
-        
+        address _owner;
+        mapping (bytes4 => address) _facets;
+        mapping (address => bytes4[]) _facetFunctionSelectors;
+        bytes4[] _selectors;
     }
+
+    
+
 }
