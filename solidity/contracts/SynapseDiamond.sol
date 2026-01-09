c92c29da8d263031cb3a9548f670ba89ff54b925
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-09 23:30:52 +0100

    update(contract): added contract diamond struct

---
+++ solidity/contracts/SynapseDiamond.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+import {SynapseUtils} from "./libs/SynapseUtils.sol";
+contract SynapseDiamond{
+    using SynapseUtils for SynapseUtils.DiamondStorage;
+
+    constructor(address diamondCut){
+
+    }
+}
---
+++ solidity/contracts/libs/SynapseUtils.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+library SynapseUtils{
+    bytes32 constant SYNAPSE_STORAGE = keccak256("Synapse.diamond.storage");
+    struct DiamonsStorage {
+        
+    }
+}
