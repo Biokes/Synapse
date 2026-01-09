e23e97612956afd9100164030a164e6501cc3ad7
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-10 00:05:03 +0100

    update(contracts): added diamondCut mutator

--- solidity/contracts/libs/SynapseUtils.sol
+++ solidity/contracts/libs/SynapseUtils.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+import {IDiamond} from "../interfaces/IDiamond.sol";
 
 library SynapseUtils{
 
@@ -16,6 +17,27 @@ library SynapseUtils{
         bytes32 position = SYNAPSE_DIAMOND_STORAGE;
         assembly {
             ds.slot := position
+        }
+    }
+    
+    function diamondCut(IDiamond.FacetCut[] memory _diamondCut,address _init,bytes memory _calldata) internal {
+        DiamondStorage storage ds = getDiamondStorage();
+        for (uint256 i = 0; i < _diamondCut.length; i++) {
+            IDiamond.FacetCut memory facetCut = _diamondCut[i];
+            address facet_address = facetCut.facetAddress;
+            IDiamond.FacetCutAction action = facetCut.action;
+            if (action == IDiamond.FacetCutAction.Add) {
+                addFacet(ds, facet_address, facetCut.functionSelectors);
+            } else if (action == IDiamond.FacetCutAction.Replace) {
+                replaceFacet(ds, facet_address, facetCut.functionSelectors);
+            } else if (action == IDiamond.FacetCutAction.Remove) {
+                removeFacet(ds, facet_address, facetCut.functionSelectors);
+            }
+        }
+
+        if (_init != address(0)) {
+            (bool success,) = _init.delegatecall(_calldata);
+            require(success, "Diamodcut Initialization failed");
         }
     }
 
