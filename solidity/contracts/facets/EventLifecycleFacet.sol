7423df67596ab571815780f194d7e1076e9e6727
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-12 21:32:55 +0100

    Implement createEvent function

--- solidity/contracts/facets/EventLifecycleFacet.sol
+++ solidity/contracts/facets/EventLifecycleFacet.sol
@@ -19,5 +19,22 @@ contract EventLifecycleFacet {
     event EventSettling(uint256 indexed eventId);
     event EventFinalized(uint256 indexed eventId, uint256 payoutAmount);
 
-    
+    function createEvent(uint64 startTime, uint64 endTime) external returns (uint256 eventId) {
+        if (startTime >= endTime) revert InvalidTimeRange();
+        
+        SynapseLibrary.AppStorage storage s = SynapseLibrary.diamondStorage();
+        
+        eventId = s.nextEventId++;
+        
+        s.events[eventId] = SynapseLibrary.Event({
+            organizer: msg.sender,
+            startTime: startTime,
+            endTime: endTime,
+            state: SynapseLibrary.STATE_CREATED,
+            escrowBalance: 0
+        });
+        
+        emit EventCreated(eventId, msg.sender, startTime, endTime);
+    }
+
 }
