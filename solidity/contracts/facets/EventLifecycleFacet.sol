efa22f8eeba2a4a796240b61f3ede9193ce8925c
Biokes <106444765+Biokes@users.noreply.github.com>
2026-01-12 21:28:50 +0100

    Add custom errors for validation and access control

--- solidity/contracts/facets/EventLifecycleFacet.sol
+++ solidity/contracts/facets/EventLifecycleFacet.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.30;
 
 import {SynapseLibrary} from "../libs/SynapseLibrary.sol";
 
+error NotOrganizer();
+error InvalidTimeRange();
+error InvalidState();
+error InvalidAmount();
+error InvalidTimestamp();
+error TransferFailed();
+error ReentrancyGuard();
+
 contract EventLifecycleFacet {
+
     
 }
